54b088d57d641cccb0af4f7959c79c0e375dbb0e
Hifumi <hifumi@peroro.club>
2023-10-07 14:56:01 -0400

    Vault update: 2023-10-07 14:56:01
    Affected files:
    .obsidian/workspace.json
    docs/notes/🎓Secondary 5/Français/La dame aux Camélias.md
    flake.nix

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           shellHook = ''
             python -m venv .venv
             source .venv/bin/activate
-            pip install -r requirements.txt
+            pip install -qr requirements.txt
           '';
         };
       };
